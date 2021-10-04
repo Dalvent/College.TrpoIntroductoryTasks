83b06d6a15e92844f1efe614a4309adcdfc20cad
Dmitrii <drozdovdm27@gmail.com>
2021-10-05 00:28:11 +0300

    Add core to gitignore

--- Trpo/Core/IEquation.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Trpo_task_1.Core
-{
-    public interface IEquation
-    {
-        double[] Solve(double a, double b, double c);
-    }
-}
--- Trpo/Core/ILog.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Trpo_task_1.Core
-{
-    public interface ILog
-    {
-        void Log(string str);
-        void Write();
-    }
-}
