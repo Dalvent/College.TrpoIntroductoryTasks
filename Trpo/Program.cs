054e7a9a7065a843dc41584d86d53dbe2eb080f6
Dmitrii <drozdovdm27@gmail.com>
2021-10-04 23:56:48 +0300

    Lab2

--- Trpo/Program.cs
+++ Trpo/Program.cs
@@ -1,39 +1,65 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Trpo_task_1
 {
     public class A
     {
+        public double CashedLinearEquationX { get; set; }
+
+        public double LinearEquation(double a, double b)
+        {
+            if (a == 0)
+                throw new ArgumentException("a can't be zero!");
+
+            CashedLinearEquationX = -b / a;
+            return CashedLinearEquationX;
+        }
     }
 
     public class B : A
     {
-        public object Value1 { get; set; }
+        public double[] CashedSquareEquationXs { get; set; }
+
+        public double[] SquareEquation(double a, double b, double c)
+        {
+            CashedSquareEquationXs = CalculateSquareEquitation(a, b, c);
+            return CashedSquareEquationXs;
+        }
+
+        private double[] CalculateSquareEquitation(double a, double b, double c)
+        {
+            if (a == 0)
+                return new[] { LinearEquation(b, c) };
+
+            double discriminant = Discriminant(a, b, c);
+
+            if (discriminant < 0)
+                return Array.Empty<double>();
+
+            if (discriminant == 0)
+                return new[] { (-b) / (2 * a) };
+
+            var sqrtDiscriminant = Math.Sqrt(discriminant);
+            return new[] { (-b + sqrtDiscriminant) / (2 * a), (-b - sqrtDiscriminant) / (2 * a) };
+        }
+
+
+        protected double Discriminant(double a, double b, double c)
+        {
+            return b * b - 4 * a * c;
+        }
     }
 
-    public class C : B
-    {
-        public object Value2 { get; set; }
-    }
-    
     public class Program
     {
         static void Main(string[] args)
         {
-            var a1 = new A();
+            var a = new B();
 
-            var b1 = new B();
-            var b2 = new B();
-            var b3 = new B();
-
-            var c1 = new C();
-
-            b1.Value1 = a1;
-            b2.Value1 = b3;
-            b3.Value1 = a1;
-
-            c1.Value1 = b1;
-            c1.Value2 = b2;
+            double linearEquationResult = a.LinearEquation(1.0, 2.0);
+            var squareEquationResult = a.SquareEquation(1.0, 2.2, 3.2);
         }
     }
 }
